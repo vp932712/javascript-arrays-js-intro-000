e52cefba44145935dafadb5447d4cdc77110fafe
Vaibhav Patel <vp932712@gmail.com>
2017-11-13 19:54:23 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -3,6 +3,7 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles" ];
 function addElementToBeginningOfArray(array, element){
   var empty = []
 for ( var i = 0; i < array.length; i ++){
+  empty.push(element);
   empty.push(array[i]);
 }
   return empty;
@@ -11,5 +12,3 @@ function destructivelyAddElementToBeginningOfArray(array, element){
   arrya.unshift(element)
   return array;
 }
-
-
